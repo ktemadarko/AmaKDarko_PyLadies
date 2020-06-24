4927d538b577e3ab64ee7002c2143f1eb44e6217
ktemadarko <k.temadarko@gmail.com>
2020-06-24 22:52:08 +0000

    Changes from 5 to 8 characters

--- AmaKDarko_Mini_Project.py
+++ AmaKDarko_Mini_Project.py
@@ -1,27 +1,47 @@
-import re
-
 def srch(x):
-    pattern=r'[A-Z][a-z][a-z][a-z][0-9]'
+    pattern=r'[A-Z][a-z][a-z][a-z][a-z][a-z][a-z][0-9]'
     
     if re.search(pattern,x):
         print(f"Great, your password is {x}!")
         
     else:
         print("\nError!\n"
-              "\nPassword should have 5 characters"
+              "\nPassword should have 8 characters"
               "\nBegin with a uppercase or capital letter\n"
-              '\nFollowed by 3 lowercase letters'
+              '\nFollowed by 6 lowercase letters'
               '\nEnd with a number from 0 to 9.')
-              
         passw=str(input("Enter a new password:"))
         srch(passw)
         
-def checkp():
+def checkpass():
     passw=str(input('What is your password?'))
-    while len(passw)!=5:
-        passw=str(input("Enter a password with 5 characters only:"))
-        if len(passw)==5:
+    while len(passw)!=8:
+        passw=str(input("Enter a password with 8 characters only:"))
+        if len(passw)==8:
             print("Checking validity of password...")
             
     srch(passw)
-checkp()
+checkpass()def srch(x):
+    pattern=r'[A-Z][a-z][a-z][a-z][a-z][a-z][a-z][0-9]'
+    
+    if re.search(pattern,x):
+        print(f"Great, your password is {x}!")
+        
+    else:
+        print("\nError!\n"
+              "\nPassword should have 8 characters"
+              "\nBegin with a uppercase or capital letter\n"
+              '\nFollowed by 6 lowercase letters'
+              '\nEnd with a number from 0 to 9.')
+        passw=str(input("Enter a new password:"))
+        srch(passw)
+        
+def checkpass():
+    passw=str(input('What is your password?'))
+    while len(passw)!=8:
+        passw=str(input("Enter a password with 8 characters only:"))
+        if len(passw)==8:
+            print("Checking validity of password...")
+            
+    srch(passw)
+checkpass()
